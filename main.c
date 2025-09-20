6d033675e47027d1b6ff6e092e404091ffcd1403
Abdul Rahman <abdulrahman42908@gmail.com>
2025-09-20 16:47:23 -0400

    Few more functions added, I think this should be good enough for a rudimentary implementation

--- main.c
+++ main.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <memory.h>
 #include "makers/makers.h"
+
+#define BOARD_WITH_WALLS 0b0010000000000100
+#define FILLED_ROW 0b0011111111111100
 
 typedef enum {
     O,
@@ -15,7 +20,7 @@ typedef enum {
 
 void init_game_board(uint16_t board[21]) {
     for (int i = 0; i < 20; i++) {
-        board[i] = 0b0010000000000100;
+        board[i] = BOARD_WITH_WALLS;
     }
 
     board[20] = 0b1111111111111111;
@@ -50,36 +55,59 @@ void piece_board_init(uint16_t piece[4][21], piece_t piece_type) {
     }
 }
 
-void drop_piece(uint16_t piece[4][21], int selected_rot) {
-    int piece_top = 0;
-    int piece_bottom = 0;
+void shift_board(uint16_t board[21], uint16_t scratch_buffer[21], bool is_game_board) {
+    memcpy(scratch_buffer, board, 21 * sizeof(uint16_t));
+    for (int i = 20; i > 0; i--) {
+        scratch_buffer[i] = scratch_buffer[i - 1];
+    }
 
-    for (int board_i = 0; board_i < 21; board_i++) {
-        if (piece[selected_rot][board_i]) {
-            piece_top = board_i;
-            break;
+    if (is_game_board) {
+        scratch_buffer[0] = BOARD_WITH_WALLS;
+        scratch_buffer[20] = 0b1111111111111111;
+    } else {
+        scratch_buffer[0] = 0b0000000000000000;
+    }
+}
+
+bool test_intersection(uint16_t board[21], uint16_t piece_board[21]) {
+    for (int i = 0; i < 21; i++) {
+        if (board[i] & piece_board[i]) {
+            return true;
         }
     }
 
-    for (int board_i = piece_top; board_i < 21; board_i++) {
-        if (!piece[selected_rot][board_i]) {
-            piece_bottom = board_i - 1;
-            break;
+    return false;
+}
+
+bool fall(uint16_t board[21], uint16_t piece[4][21], int selected_rot) {
+    uint16_t scratch_piece[21] = {0};
+    shift_board(piece[selected_rot], scratch_piece, false);
+    if (test_intersection(board, scratch_piece)) {
+        return false;
+    }
+
+    memcpy(piece[selected_rot], scratch_piece, 21 * sizeof(uint16_t));
+    return true;
+}
+
+void clear_rows(uint16_t board[21]) {
+    int dst = 19;
+    for (int src = 19; src >= 0; src--) {
+        if ((board[src] & FILLED_ROW) != FILLED_ROW) {
+            board[dst--] = board[src];
         }
     }
 
-    /* Future plans for this function: Go through the piece things. Bitwise & them shifted down by 1
-       and if that returns true, that means there is a collision and THAT means that the piece cannot be
-       moved further. Only check this for the SELECTED PIECE. */
+    while (dst >= 0) {
+        board[dst--] = BOARD_WITH_WALLS;
+    }
 }
 
 int main(int argc, char** argv) {
     uint16_t board[21] = {0};
     uint16_t piece[4][21] = {0};
     /* Move legality detection: Just run logical & on potential move and make it if it is 0 :) */
-    /* Bottom of piece board is made solid for easier detection */
-    for (int i = 0; i < 4; i++)
-        piece[i][20] = 0b1111111111111111;
+    /* Bottom of board board is made solid for easier detection */
 
     int selected_rot = 0; /* there will be 4 piece boards: you must select whichever you are using */
 
