e9267e64b6abf86a6824085ac57ea5600a6f68e8
Abdul Rahman <abdulrahman42908@gmail.com>
2025-09-21 19:46:33 -0400

    terry davis

--- include/stdmac.h
+++ include/stdmac.h
@@ -12,7 +12,7 @@
 #define U8 unsigned char
 #define I16 int
 #define I8 signed char
-#define U0 U0
+#define U0 void
 #define BOARD_WITH_WALLS 0b0010000000000100
 #define FILLED_ROW 0b0011111111111100
 #define BOARD_HEIGHT 21
